b3e1da6720fcb0dc417c4f28434da6650d3ff6bb
Nicolas Agnese <na@huntmads.com>
2013-10-21 23:26:37 -0300

    Add mercadopago in config of books for libro1

--- resources/config/books.php
+++ resources/config/books.php
@@ -12,6 +12,17 @@ $app['feedback.options'] = array(
 // Libros
 $app['libro1'] = array(
 	'precio' => "5.00",
+	'mercadopago' => array(
+		"items" => array(
+			array(
+			    "title" => "Plan de recetas semanal",
+			    "quantity" => 1,
+			    "currency_id" => "USD",
+			    "unit_price" => 5.00
+			)
+		),
+		"external_reference" => 'Replaced with session_id on RunTime',
+	)
 );
 
 ?>
