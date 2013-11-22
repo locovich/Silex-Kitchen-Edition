f33c47c70d65fe098a647111f5713a95ac68f725
Nicolas Agnese <na@huntmads.com>
2013-11-22 00:36:39 -0300

    Changes in indexes regarding credentials

--- web/index.php
+++ web/index.php
@@ -6,9 +6,9 @@ Symfony\Component\Debug\Debug::enable();
 
 $app = new Silex\Application();
 
+require __DIR__.'/../resources/config/credentials.php'; // <-- Not in github, where I place my credentials to other systems API's
 require __DIR__.'/../resources/config/dev.php';
 require __DIR__.'/../resources/config/books.php';
-require __DIR__.'/../resources/config/credentials.php'; // <-- Not in github, where I place my credentials to other systems API's
 require __DIR__.'/../src/app.php';
 
 //require __DIR__.'/../src/controllers.php';
--- web/index_prod.php
+++ web/index_prod.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require __DIR__.'/../resources/config/credentials.php'; // <-- Not in github, where I place my credentials to other systems API's
 require __DIR__.'/../resources/config/prod.php';
 require __DIR__.'/../resources/config/books.php';
 require __DIR__.'/../src/app.php';
